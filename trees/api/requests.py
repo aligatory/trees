af877e173ed603d852b67ac048ae2c9f9269e096
evgeny <tyztuz@gmail.com>
2020-05-06 16:42:03 +0500

    wnat grow field now not required

--- trees/api/requests.py
+++ trees/api/requests.py
@@ -1,4 +1,5 @@
 from http import HTTPStatus
+from typing import Optional
 
 from fastapi import APIRouter, Form
 from fastapi.responses import RedirectResponse
@@ -14,8 +15,8 @@ success = 'http://185.189.14.105/success'
 
 @router.get('/')
 async def create_request(last_name: str, first_name: str, patronymic: str,
-                         phone_number: str, wants_to_grow: str,
-                         request_type: str):
+                         phone_number: str,
+                         request_type: str, wants_to_grow: Optional[str] = None):
     response: Response = requests.post(f'{itsm_base}create/serviceCall$growingTree?accessKey={access_key}',
                                        json={'metaClass': 'serviceCall$growingTree',
                                              'agreement': 'agreement$2994803',
@@ -23,7 +24,7 @@ async def create_request(last_name: str, first_name: str, patronymic: str,
                                              'lastName': last_name,
                                              'firstName': first_name,
                                              'treeId': 1,
-                                             "isCitizenWants": True if wants_to_grow == 'on' else False,
+                                             "isCitizenWants": True if wants_to_grow else False,
                                              'telephone': phone_number,
                                              'patronymic': patronymic})
     if response.status_code == HTTPStatus.CREATED:
