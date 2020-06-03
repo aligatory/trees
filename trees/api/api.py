5af9b254f3f6041a78f983bf7f53db565f2ca812
evgeny <tyztuz@gmail.com>
2020-06-03 13:26:51 +0500

    added user checking before creating new

--- trees/api/api.py
+++ trees/api/api.py
@@ -9,7 +9,7 @@ from trees.url import base_url
 
 router = APIRouter()
 itsm_base = 'https://training-1.itsm365.com/sd/services/rest'
-access_key = 'd766a952-de5b-4b00-a209-a16954eac7e7'
+access_key = 'f2f348c8-25c0-4fb4-b819-fc99d421aca7'
 
 success = f'{base_url}/success'
 add_photo_url = f"{itsm_base}/add-file"
@@ -17,28 +17,35 @@ add_photo_url = f"{itsm_base}/add-file"
 
 @router.post('/')
 async def create_request(
-    last_name: str = Form(...),
-    first_name: str = Form(...),
-    phone_number: str = Form(...),
-    email: str = Form(...),
-    photo: UploadFile = File(...),
+        last_name: str = Form(...),
+        first_name: str = Form(...),
+        phone_number: str = Form(...),
+        email: str = Form(...),
+        photo: UploadFile = File(...),
 ):
-    response1: Response = requests.post(
-        f'{itsm_base}/create-m2m/employee$contactPerson?accessKey={access_key}',
-        json={
-            'metaClass': 'employee$contactPerson',
-            'parent': 'ou$3246521',
-            'lastName': last_name,
-            'firstName': first_name,
-            'email': email,
-        },
-    )
+    response123: Response = requests.post(f'{itsm_base}/find/employee$contactPerson?accessKey={access_key}', json={
+        'email': email
+    })
+    if len(response123.json()) == 0:
+        response1: Response = requests.post(
+            f'{itsm_base}/create-m2m/employee$contactPerson?accessKey={access_key}',
+            json={
+                'metaClass': 'employee$contactPerson',
+                'parent': 'ou$3246521',
+                'lastName': last_name,
+                'firstName': first_name,
+                'email': email,
+            },
+        )
+        uuid = response1.json()['UUID']
+    else:
+        uuid = response123.json()[0]['UUID']
 
     response: Response = requests.post(
         f'{itsm_base}/create-m2m/serviceCall$complaint?accessKey={access_key}',
         json={
             'metaClass': 'serviceCall$complaint',
-            'client': response1.json()['UUID'],
+            'client': uuid,
             'service': 'slmService$3269602',
             'agreement': 'agreement$605301',
             'shortDescr': 'Жалоба на незаконный спил',
