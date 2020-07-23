0a29e39d7fa53a34aa4e24f9dca239a17ce8955c
Bulygin Evgeny <ebulygin@ussc.ru>
2020-07-23 17:59:23 +0500

    fixed

--- trees/config.py
+++ trees/config.py
@@ -4,7 +4,7 @@ from pydantic import BaseSettings
 class ServiceSettings(BaseSettings):
     address: str = "localhost"
     port: int = 8000
-    access_key: str
+    access_key: str = ""
 
 
 service_settings = ServiceSettings()
